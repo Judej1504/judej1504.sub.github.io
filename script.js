f0e2525907188222ef9994eea0330451786b3ea7
Judej1504 <judej1504@gmail.com>
2025-12-23 19:09:38 +0000

    Refactor script.js to improve functionality and structure

--- script.js
+++ script.js
@@ -1,163 +1,94 @@
-const params = new URLSearchParams(window.location.search);
-var id = params.get("id") || "UCX6OQ3DkcsbYNE6H8uQQuVA";
-var url = `https://ests.sctools.org/api/get/`; 
-
-const chart = new Highcharts.chart({
-	chart: {
-		renderTo: "chart",
-		type: "line",
-		zoomType: "x",
-		backgroundColor: "transparent",
-		plotBorderColor: "transparent",
-		height: "-11px",
-		animation: false,
-		style: {
-			fontFamily: "Roboto",
-		},
-	},
-	title: {
-		text: "",
-	},
-	xAxis: {
-		type: "datetime",
-		tickPixelInterval: 500,
-		labels: {
-			style: {
-				color: "#AAAAAA",
-			},
-		},
-		gridLineColor: "#9E9E9E",
-		lineColor: "#9E9E9E",
-		minorGridLineColor: "#858585",
-		tickColor: "#858585",
-		title: {
-			style: {
-				color: "#858585",
-			},
-		},
-	},
-	yAxis: {
-		title: {
-			text: "",
-		},
-		labels: {
-			style: {
-				color: "#AAAAAA",
-			},
-			formatter: function() {
-				function abbreviate(count, withAbbr = true, decimals = 2) {
-					if (String(count)[0] === "0") {
-						if (count === 0) return "0";
-						else return count.toFixed(decimals);
-					}
-
-					let neg = false;
-					if (String(count)[0] == "-") {
-						neg = true;
-						count = ~Number(count) + 1;
-					}
-
-					const COUNT_ABBRS = ["", "K", "M", "B"];
-					const i =
-						count === 0 ? count : Math.floor(Math.log(count) / Math.log(1000));
-					let result = parseFloat(
-						(count / Math.pow(1000, i)).toFixed(decimals)
-					).toString();
-					if (withAbbr) result += `${COUNT_ABBRS[i]}`;
-					if (neg) result = `-${result}`;
-					return result;
-				}
-
-				return abbreviate(this.value);
-			},
-		},
-		gridLineColor: "#3D3D3D",
-		lineColor: "#3D3D3D",
-		minorGridLineColor: "#3D3D3D",
-		tickColor: "#3D3D3D",
-		opposite: true,
-	},
-	credits: {
-		enabled: false,
-	},
-	tooltip: {
-		shared: true,
-		formatter: function() {
-			// @ts-ignore
-			var index = this.points[0].series.xData.indexOf(this.x);
-			// @ts-ignore
-			var lastY = this.points[0].series.yData[index - 1];
-			// @ts-ignore
-			var dif = this.y - lastY;
-			var r =
-				// @ts-ignore
-				Highcharts.dateFormat("%A %b %e, %H:%M:%S", new Date(this.x)) +
-				'<br><span style="color:black">\u25CF </span>' +
-				// @ts-ignore
-				this.points[0].series.name +
-				": <b>" +
-				// @ts-ignore
-				Highcharts.numberFormat(this.y, 0);
-			if (dif < 0) {
-				r +=
-					'<span style="color:#ff0000;font-weight:bold;"> (' +
-					Highcharts.numberFormat(dif, 0) +
-					")</span>";
-			}
-			if (dif > 0) {
-				r +=
-					'<span style="color:#00bb00;font-weight:bold;"> (+' +
-					Highcharts.numberFormat(dif, 0) +
-					")</span>";
-			}
-			return r;
-		},
-	},
-	series: [
-		{
-			showInLegend: false,
-			name: "",
-			marker: { enabled: false },
-			color: "#3FABCD",
-			lineColor: "#3FABCD",
-			lineWidth: 2,
-		},
-	],
-});
-
-function getdata(a) {
-	fetch(url + a)
-		.then((res) => res.json())
-		.then((data) => {
-document.getElementById('c').innerHTML = data.stats.estCount;	
-			document.getElementById("avatar").src = data.info.avatar;
-			document.getElementById("title").textContent = data.info.name;
-			if (chart.series[0].points.length >= 3600)
-				chart.series[0].data[0].remove();
-			chart.series[0].addPoint([Date.now(), data.items[0].stats.estCount]);
-		});
+function GetGoal(count2) {
+  var count = parseFloat(count2);
+  var t = parseFloat(count2);
+  if (count == null) return 0;
+  if (10 > t) return 10 - t;
+  var e = "" + t;
+  return Math.abs(
+    t -
+      (e.length > 6
+        ? 1e6 * (Math.floor(t / 1e6) + 1)
+        : (parseInt(e.charAt(0)) + 1) * Math.pow(10, e.length - 1))
+  );
 }
 
-setTimeout(() => {
-	getdata(id);
-}, 100);
+function GetGoal2(count2) {
+  var count = parseFloat(count2);
+  var t = parseFloat(count2);
+  if (count == null) return 0;
+  if (10 > t) return 10;
+  var e = "" + t;
+  return e.length > 6
+    ? 1e6 * (Math.floor(t / 1e6) + 1)
+    : (parseInt(e.charAt(0)) + 1) * Math.pow(10, e.length - 1);
+}
 
-setTimeout(() => {
-	getdata(id);
-}, 500);
+function GetGoalText(t) {
+  return ("" + t).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id") || "UCX6OQ3DkcsbYNE6H8uQQuVA";
+document.getElementById("imageLink").href = `https://youtube.com/channel/${id}`;
+document.getElementById(
+  "subscribeBtn"
+).href = `https://youtube.com/channel/${id}?sub_confirmation=1`;
+
 setInterval(() => {
-	getdata(id);
+  fetch(`https://ests.sctools.org/api/get/`)
+    .then((res) => res.json())
+    .then((data) => {
+      document.getElementById("name").textContent = info.name;
+      document.querySelector(
+        '[data-icon="zondicons:checkmark"]'
+      ).style.display = data.isStudio ? "block" : "none";
+
+      const image = document.getElementById("image");
+      image.src = 
+        data.snippet.thumbnails.high.url ||
+        data.snippet.thumbnails.medium.url ||
+        data.snippet.thumbnails.default.url;
+      image.alt = info.avatar;
+      document.getElementById("subscribers").innerHTML = stats.estCount;
+      document.getElementById("goal").innerHTML = GetGoal(data.estSubCount);
+      document.getElementById(
+        "goalText"
+      ).textContent = `subscribers to ${GetGoalText(
+        GetGoal2(data.estSubCount)
+      )}`;
+    });
 }, 2000);
 
+function toggleLightMode() {
+  document.body.classList.toggle("light");
+
+  const localTheme = localStorage.getItem("theme");
+  if (!localTheme || localTheme === "dark")
+    localStorage.setItem("theme", "light");
+  else localStorage.setItem("theme", "dark");
+}
+
+window.onload = () => {
+  const localTheme = localStorage.getItem("theme");
+  if (!localTheme) {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      localStorage.setItem("theme", "dark");
+    } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+      document.body.classList.toggle("light");
+      localStorage.setItem("theme", "light");
+    }
+  }
+  if (localTheme === "light") document.body.classList.toggle("light");
+};
+
 function search() {
-	const prompt = window.prompt("Enter channel name, ID, or URL.");
-	if (prompt)
-		fetch(
-			`https://mixerno.space/api/youtube-channel-counter/search/${id}`
-			 
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				window.location.href = "?id=" + data[0].id;
-			});
+  const prompt = window.prompt("Enter channel name, ID, or URL.");
+  if (prompt)
+    fetch(
+      `https://subscribercount.app/api/search?platform=youtube&type=channel&query=${prompt}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        window.location.href = "?id=" + data[0].id;
+      });
 }
